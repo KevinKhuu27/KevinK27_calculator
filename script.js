66f7ee47d5d9b3f0d150e53551cac33f0f5b709f
KevinKhuu27 <khuu.kevin27@hotmail.com>
2023-08-30 19:20:53 -0400

    Added event listeners
    Added click event listeners for each button on the calculator

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let n;
-let m;
-let operation = "";
+let display_val = "";
+let answer = "";
 
 function add (n, m)
 {
@@ -34,3 +33,44 @@ function operate (n, m, operator)
         return divide(n, m);
 }
 
+const oneKey = document.getElementById("1");
+const twoKey = document.getElementById("2");
+const threeKey = document.getElementById("3");
+const fourKey = document.getElementById("4");
+const fiveKey = document.getElementById("5");
+const sixKey = document.getElementById("6");
+const sevenKey = document.getElementById("7");
+const eightKey = document.getElementById("8");
+const nineKey = document.getElementById("9");
+const zeroKey = document.getElementById("0");
+
+const xKey = document.getElementById("x");
+const divKey = document.getElementById("div");
+const plusKey = document.getElementById("+");
+const minusKey = document.getElementById("-");
+
+const decimalKey = document.getElementById(".");
+const expKey = document.getElementById("exp");
+const delKey = document.getElementById("del");
+const clearKey = document.getElementById("clear");
+const ansKey = document.getElementById("ans");
+const equalKey = document.getElementById("=");
+
+oneKey.addEventListener('click', () => updateDisplay("1"));
+twoKey.addEventListener('click', () => updateDisplay("2"));
+threeKey.addEventListener('click', () => updateDisplay("3"));
+fourKey.addEventListener('click', () => updateDisplay("4"));
+fiveKey.addEventListener('click', () => updateDisplay("5"));
+sixKey.addEventListener('click', () => updateDisplay("6"));
+sevenKey.addEventListener('click', () => updateDisplay("7"));
+eightKey.addEventListener('click', () => updateDisplay("8"));
+nineKey.addEventListener('click', () => updateDisplay("9"));
+zeroKey.addEventListener('click', () => updateDisplay("0"));
+
+xKey.addEventListener('click', () => updateDisplay("*"));
+divKey.addEventListener('click', () => updateDisplay("/"));
+plusKey.addEventListener('click', () => updateDisplay("+"));
+minusKey.addEventListener('click', () => updateDisplay("-"));
+
+clearKey.addEventListener('click', () => clearAll());
+equalKey.addEventListener('click', () => determineEquation());
